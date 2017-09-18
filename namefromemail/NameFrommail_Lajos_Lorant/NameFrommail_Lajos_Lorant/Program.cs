ef09cac13f715bf660ab3fbfec24088041f86924
Lorant Lajos <trojanosall@gmail.com>
2017-09-18 17:39:47 +0200

    NameFrommail - preparation of Funcion

---
+++ namefromemail/NameFrommail_Lajos_Lorant/NameFrommail_Lajos_Lorant/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameFrommail_Lajos_Lorant
+{
+    class Program
+    {
+        public static string NameFromEmail(string Mail)
+        {
+            Console.WriteLine("Please give me your email address in the following format (firstName.lastName@exam.com): ");
+            string email = Console.ReadLine();
+            string username = email.Substring(0, email.IndexOf("@"));
+
+            string firstName = username.Substring(0, username.IndexOf("."));
+            firstName = char.ToUpper(firstName[0]) + firstName.Substring(1);
+
+            string lastName = username.Substring(username.IndexOf(".") + 1);
+            lastName = char.ToUpper(lastName[0]) + lastName.Substring(1);
+
+            Console.WriteLine(lastName + " " + firstName);
+
+            return NameFromEmail(string);
+        }
+
+        static void Main(string[] args)
+        {
+
+            // Create a function that takes an email address as input in the following format: firstName.lastName@exam.com
+            //and returns a string that represents the user name in the following format: lastName firstName
+            //example: "elek.viz@exam.com" for this input the output should be: "Viz Elek"
+            //accents does not matter 
+            Console.WriteLine(NameFromEmail("elek.viz@exam.com"));
+            Console.ReadLine();
+
+        }
+    }
+}
